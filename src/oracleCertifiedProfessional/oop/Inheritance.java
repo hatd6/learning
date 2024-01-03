6b00f2f627e5567cf799497ec409042d2dae6603
Hammer <tranduyha2000@gmail.com>
2024-01-03 21:24:59 +0700

    add file config eclipse

---
+++ src/oracleCertifiedProfessional/oop/Inheritance.java
@@ -0,0 +1,31 @@
+/*
+ * GUMBOX BLUE CORPORATION.
+ * Copyright (c) 2023-present Gumbox Blue, Corporation. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Gumbox Blue Corporation.
+ * ("Confidential Information"). You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement you entered
+ * into with Gumbox Blue Corporation.
+ *
+ */
+package oracleCertifiedProfessional.oop;
+
+/**
+ *The inherited fields can be used directly, just like any other fields.
+ *You can declare a field in the subclass with the same name as the one in the superclass, thus hiding it (not recommended).
+ *You can declare new fields in the subclass that are not in the superclass.
+ *The inherited methods can be used directly as they are.
+ *You can write a new instance method in the subclass that has the same signature as the one in the superclass, thus overriding it.
+ *You can write a new static method in the subclass that has the same signature as the one in the superclass, thus hiding it.
+ *You can declare new methods in the subclass that are not in the superclass.
+ *You can write a subclass constructor that invokes the constructor of the superclass, either implicitly or by using the keyword super.
+ *The following sections in this lesson will expand on these topics.
+
+ * @author Hammer
+ */
+public class Inheritance {
+
+    void toStr() {
+
+    }
+}
---
+++ src/oracleCertifiedProfessional/oop/SubClass.java
@@ -0,0 +1,23 @@
+/*
+ * GUMBOX BLUE CORPORATION.
+ * Copyright (c) 2023-present Gumbox Blue, Corporation. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Gumbox Blue Corporation.
+ * ("Confidential Information"). You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement you entered
+ * into with Gumbox Blue Corporation.
+ *
+ */
+package oracleCertifiedProfessional.oop;
+
+/**
+ * @author Hammer
+ */
+
+public class SubClass extends Inheritance {
+
+    @Override
+    void toStr() {
+
+    }
+}
